9f709e4e29e80674e168a3764031c8e7cbc65fb7
ZSJ <1940982510@qq.com>
2021-04-08 11:27:27 +0800

    获取进一步url

--- guazi/spiders/spiderGuazi.py
+++ guazi/spiders/spiderGuazi.py
@@ -9,4 +9,17 @@ class SpiderguaziSpider(scrapy.Spider):
     def parse(self, response):
         title = response.xpath('//html/head/title/text()')
         print(title)
+        url = response.xpath('/html/body/div[6]/ul/li[1]/a/@href').get()
+        print(url)
+        for i in range(1, 41):
+            print(i)
+            url = response.xpath('/html/body/div[6]/ul/li[' + str(i) + ']/a/@href').get()
+            print(url)
+
+
+        # lists = response.xpath('//ul[@class="carlist clearfix js-top"]')
+        # print(lists)
+        # for i in lists:
+        #     url = i.xpath('./li/a/@href').get()
+        #     print("url:" + url)
         pass
